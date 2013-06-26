f684183d5c36d9eea4c1b4a78f91342f0d6c2a82
Didier Roche <didrocks@ubuntu.com>
2013-06-26 11:01:06 +0200

    install python autopilot tests using a real python installation (needed if we transition from one python version to another)

---
+++ tests/autopilot/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='unity',
+    version='8.0',
+    description='Unity 8 autopilot tests.',
+    url='https://launchpad.net/unity',
+    license='GPLv3',
+    packages=find_packages(),
+)
