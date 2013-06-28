ca130cefd57591fde419ac9f2975c2fa63e57162
Michael Zanetti <michael.zanetti@canonical.com>
2013-06-28 10:18:52 +0200

    fix license header for setup.py

--- tests/autopilot/setup.py
+++ tests/autopilot/setup.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python
 
+# -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
+# Copyright 2013 Canonical
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3, as published
+# by the Free Software Foundation.
+
+
 from distutils.core import setup
 from setuptools import find_packages
 
