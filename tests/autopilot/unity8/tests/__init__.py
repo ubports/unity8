af16994294ed1694161f553a5132ae13da7135b6
Michał Sawicz <michal.sawicz@canonical.com>
2013-07-03 20:53:13 +0200

    Fix environment for autopilot tests.

--- tests/autopilot/unity8/tests/__init__.py
+++ tests/autopilot/unity8/tests/__init__.py
@@ -8,6 +8,7 @@
 """unity8 autopilot tests."""
 
 import os.path
+import sysconfig
 
 from autopilot.testcase import AutopilotTestCase
 from autopilot.matchers import Eventually
@@ -23,6 +24,7 @@ class ShellTestCase(AutopilotTestCase):
 
     """A common test case class that provides several useful methods for shell tests."""
 
+    libdir = "/usr/lib/{0}/unity8".format(sysconfig.get_config_var('MULTIARCH'))
     lightdm_mock = "full"
 
     def setUp(self, geometry, grid_size):
@@ -39,7 +41,8 @@ class ShellTestCase(AutopilotTestCase):
             self.launch_test_local(geometry)
 
     def launch_test_local(self, geometry):
-        os.environ['LD_LIBRARY_PATH'] = "../../builddir/tests/mocks/libusermetrics:../../builddir/tests/mocks/LightDM/" + self.lightdm_mock
+        os.environ['LD_LIBRARY_PATH'] = "../../builddir/tests/mocks/libusermetrics:../../builddir/tests/mocks/LightDM" + self.lightdm_mock
+        os.environ['QML2_IMPORT_PATH'] = "../../builddir/tests/mocks"
         if geometry != "0x0":
             self.app = self.launch_test_application(
                 "../../builddir/unity8", "-geometry", geometry, "-frameless", app_type='qt')
@@ -48,7 +51,8 @@ class ShellTestCase(AutopilotTestCase):
                 "../../builddir/unity8", "-fullscreen", app_type='qt')
 
     def launch_test_installed(self, geometry):
-        os.environ['LD_LIBRARY_PATH'] = "/usr/share/unity8/plugins/mocks/libusermetrics:/usr/share/unity8/plugins/mocks/LightDM/" + self.lightdm_mock
+        os.environ['LD_LIBRARY_PATH'] = "{0}/qml/mocks/libusermetrics:{0}/qml/mocks/LightDM/{1}".format(self.libdir, self.lightdm_mock)
+        os.environ['QML2_IMPORT_PATH'] = "{0}/qml/mocks".format(self.libdir)
         if model() == 'Desktop' and geometry != "0x0":
             self.app = self.launch_test_application(
                "unity8", "-geometry", geometry, "-frameless", app_type='qt')
