7d8fa6de4f5abe76d3b210932ab8b587e7b80272
Michal Hruby <michal.mhr@gmail.com>
2013-07-05 13:03:45 +0100

    Fix a typo

--- plugins/Unity/categoryresults.cpp
+++ plugins/Unity/categoryresults.cpp
@@ -2,7 +2,7 @@
  * Copyright (C) 2013 Canonical, Ltd.
  *
  * Authors:
- *  Michał Hruby <michal.hruby@canonical.com>
+ *  Michal Hruby <michal.hruby@canonical.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
