c045f607d25de5f862d96b6b7291fba16483f10f
Nick Dedekind <nicholas.dedekind@gmail.com>
2013-07-10 15:45:09 +0100

    added split autopilot targets

--- cmake/modules/autopilot.cmake
+++ cmake/modules/autopilot.cmake
@@ -1,7 +1,10 @@
 add_custom_target(autopilot)
 
 function(declare_autopilot_test TEST_NAME WORKING_DIR)
-  add_custom_command(TARGET autopilot
-  COMMAND autopilot run ${TEST_NAME}
-  WORKING_DIRECTORY ${WORKING_DIR})
+    add_custom_target(autopilot-${TEST_NAME}
+        COMMAND autopilot run ${TEST_NAME}
+        WORKING_DIRECTORY ${WORKING_DIR}
+    )
+
+    add_dependencies(autopilot autopilot-${TEST_NAME})
 endfunction()
