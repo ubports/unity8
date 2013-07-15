cbf9b9aaba8f3cde42a4275b0e39c0191b2203c2
Thomi Richards <thomi.richards@canonical.com>
2013-07-15 12:40:17 +1200

    Cleaned up screen alignment test cases.

--- tests/autopilot/unity8/shell/tests/test_big_screen_alignment.py
+++ tests/autopilot/unity8/shell/tests/test_big_screen_alignment.py
@@ -7,18 +7,20 @@
 
 """Add tests here if you want to ensure everything is aligned correctly on huge screens. Because this test does not fit on most screens, you should not use input devices here or your tests are likely to fail"""
 
-from testtools.matchers import Equals
 from autopilot.matchers import Eventually
+from testtools.matchers import Equals
 
 from unity8.shell.tests import ShellTestCase
 
-class TestBig(ShellTestCase):
-    def setUp(self):
-        super(TestBig, self).setUp("2560x1600", "20")
-        self.assertThat(self.main_window.get_qml_view().visible, Eventually(Equals(True)))
 
-    def tearDown(self):
-        super(TestBig, self).tearDown()
+# TODO: Find out if these tests are still required, if so, what they're supposed
+# to be testing, and whether they're supposed to work on the devices.
+class ScreenAlignmentTests(ShellTestCase):
+    def setUp(self):
+        super(ScreenAlignmentTests, self).setUp("2560x1600", "20")
+        self.assertThat(self.main_window.get_qml_view().visible,
+            Eventually(Equals(True))
+        )
 
     def test_hud_not_shown_greeter(self):
         hud_showable = self.main_window.get_hud_showable()
