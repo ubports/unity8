8bd7c9b724b754c390a3a1476c1f5f385682c2c1
Michał Sawicz <michal.sawicz@canonical.com>
2013-07-23 00:06:33 +0200

    Run autopilot under C locale.

--- cmake/modules/autopilot.cmake
+++ cmake/modules/autopilot.cmake
@@ -2,7 +2,7 @@ add_custom_target(autopilot)
 
 function(declare_autopilot_test TEST_NAME TEST_SUITE WORKING_DIR)
     add_custom_target(autopilot-${TEST_NAME}
-        COMMAND QML2_IMPORT_PATH=${SHELL_PRIVATE_LIBDIR}/qml/mocks autopilot run ${TEST_SUITE}
+        COMMAND LANG=C QML2_IMPORT_PATH=${SHELL_PRIVATE_LIBDIR}/qml/mocks autopilot run ${TEST_SUITE}
         WORKING_DIRECTORY ${WORKING_DIR}
         DEPENDS install
     )
