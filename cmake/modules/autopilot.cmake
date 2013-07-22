559efae4e34b1382d18c53253af2810e708c4ba8
Michał Sawicz <michal.sawicz@canonical.com>
2013-07-22 23:50:23 +0200

    Tweak the CMake autopilot module.

--- cmake/modules/autopilot.cmake
+++ cmake/modules/autopilot.cmake
@@ -2,8 +2,9 @@ add_custom_target(autopilot)
 
 function(declare_autopilot_test TEST_NAME TEST_SUITE WORKING_DIR)
     add_custom_target(autopilot-${TEST_NAME}
-        COMMAND autopilot run ${TEST_SUITE}
+        COMMAND QML2_IMPORT_PATH=${SHELL_PRIVATE_LIBDIR}/qml/mocks autopilot run ${TEST_SUITE}
         WORKING_DIRECTORY ${WORKING_DIR}
+        DEPENDS install
     )
 
     add_dependencies(autopilot autopilot-${TEST_NAME})
