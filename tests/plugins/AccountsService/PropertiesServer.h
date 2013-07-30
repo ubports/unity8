447982b01960fd20dc232f21b7066fec3a3d7822
Michael Terry <michael.terry@canonical.com>
2013-07-30 13:54:03 -0400

    Add tests for AccountsService plugin

---
+++ tests/plugins/AccountsService/PropertiesServer.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2013 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3, as published
+ * by the  Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranties of
+ * MERCHANTABILITY, SATISFACTORY QUALITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 3 along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>
+ *
+ * Authored by: Michael Terry <michael.terry@canonical.com>
+ */
+
+#ifndef UNITY_PROPERTIESSERVER_H
+#define UNITY_PROPERTIESSERVER_H
+
+#include "PropertiesServer.h"
+#include <QtCore/QObject>
+#include <QtCore/QString>
+#include <QtDBus/QDBusContext>
+#include <QtDBus/QDBusVariant>
+
+class PropertiesServer: public QObject, protected QDBusContext
+{
+    Q_OBJECT
+
+public:
+    explicit PropertiesServer(QObject *parent = 0);
+
+public Q_SLOTS:
+    QDBusVariant Get(const QString &interface, const QString &property);
+    void Set(const QString &interface, const QString &property, const QDBusVariant &variant);
+
+private:
+    bool edge_demo;
+};
+
+#endif
