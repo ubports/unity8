7d72d31d62dc8bd6401ed42fe13037aa92beaea9
Michael Terry <michael.terry@canonical.com>
2013-08-21 11:46:15 -0400

    Fix racy start of AccountsService

--- plugins/AccountsService/AccountsService.cpp
+++ plugins/AccountsService/AccountsService.cpp
@@ -18,6 +18,7 @@
 
 #include "AccountsService.h"
 #include <QtDBus/QDBusConnection>
+#include <QtDBus/QDBusConnectionInterface>
 #include <QtDBus/QDBusMessage>
 #include <QtDBus/QDBusVariant>
 
@@ -26,10 +27,13 @@ AccountsService::AccountsService(QObject* parent)
     accounts_manager(NULL),
     users()
 {
+    auto connection = QDBusConnection::SM_BUSNAME();
+    auto interface = connection.interface();
+    interface->startService("org.freedesktop.Accounts");
     accounts_manager = new QDBusInterface("org.freedesktop.Accounts",
                                           "/org/freedesktop/Accounts",
                                           "org.freedesktop.Accounts",
-                                          QDBusConnection::SM_BUSNAME(), this);
+                                          connection, this);
 }
 
 QVariant AccountsService::getUserProperty(const QString &user, const QString &property)
