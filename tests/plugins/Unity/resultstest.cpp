dfe2ac7792844bc720e04807c17e395aae0a867d
Michal Hruby <michal.mhr@gmail.com>
2013-09-06 11:02:19 +0100

    Move authors to bottom of the header

--- tests/plugins/Unity/resultstest.cpp
+++ tests/plugins/Unity/resultstest.cpp
@@ -1,9 +1,6 @@
 /*
  * Copyright (C) 2013 Canonical, Ltd.
  *
- * Authors:
- *  Michal Hruby <michal.hruby@canonical.com>
- *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; version 3.
@@ -15,6 +12,9 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *  Michal Hruby <michal.hruby@canonical.com>
  */
 
 #include <QTest>
