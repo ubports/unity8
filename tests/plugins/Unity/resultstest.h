4ca9c78afea929ae0178ef4ce6d6de28ef1593ee
Michal Hruby <michal.mhr@gmail.com>
2013-09-06 09:21:03 +0100

    Add author to copyright header

--- tests/plugins/Unity/resultstest.h
+++ tests/plugins/Unity/resultstest.h
@@ -1,6 +1,9 @@
 /*
  * Copyright (C) 2013 Canonical, Ltd.
  *
+ * Authors:
+ *  Michal Hruby <michal.hruby@canonical.com>
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; version 3.
