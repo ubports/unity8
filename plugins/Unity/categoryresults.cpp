2d839af33812e6c83b62722f82fecf9d119c30a7
Michal Hruby <michal.mhr@gmail.com>
2013-10-03 18:56:22 +0100

    Added rendererHints

--- plugins/Unity/categoryresults.cpp
+++ plugins/Unity/categoryresults.cpp
@@ -46,6 +46,7 @@ CategoryResults::CategoryResults(QObject* parent)
     m_roles[CategoryResults::RoleComment] = "comment";
     m_roles[CategoryResults::RoleDndUri] = "dndUri";
     m_roles[CategoryResults::RoleMetadata] = "metadata";
+    m_roles[CategoryResults::RoleRendererHints] = "rendererHints";
 }
 
 int CategoryResults::categoryIndex() const
@@ -93,6 +94,19 @@ CategoryResults::data(const QModelIndex& index, int role) const
             return DeeListModel::data(index, ResultsColumn::DND_URI);
         case RoleMetadata:
             return DeeListModel::data(index, ResultsColumn::METADATA);
+        case RoleRendererHints:
+        {
+            QVariantHash hash(DeeListModel::data(index, ResultsColumn::METADATA).toHash());
+            if (hash.contains("content")) {
+                QVariantMap hints;
+                QVariantHash innerHash(hash["content"].toHash());
+                if (innerHash.contains("scope_disabled")) {
+                    hints["scope_disabled"] = innerHash["scope_disabled"];
+                }
+                return hints.empty() ? QVariant() : QVariant::fromValue(hints);
+            }
+            return QVariant();
+        }
         default:
             return QVariant();
     }
