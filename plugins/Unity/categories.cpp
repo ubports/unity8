461c453a9b529c05233894c3ed04aad3b39fc670
Pawel Stolowski <pawel.stolowski@canonical.com>
2013-10-14 13:39:55 +0200

    Added fixme.

--- plugins/Unity/categories.cpp
+++ plugins/Unity/categories.cpp
@@ -132,7 +132,7 @@ Categories::setUnityScope(const unity::dash::Scope::Ptr& scope)
     m_signals.disconnectAll();
     m_signals << m_unityScope->categories()->model.changed.connect(sigc::mem_fun(this, &Categories::onCategoriesModelChanged));
 
-    // Don't handle category order changes for now as it causes UI issues (https://bugs.launchpad.net/unity8/+bug/1239584).
+    // FIXME: Don't handle category order changes for now as it causes UI issues (https://bugs.launchpad.net/unity8/+bug/1239584).
     //m_signals << m_unityScope->category_order.changed.connect(sigc::mem_fun(this, &Categories::onCategoryOrderChanged));
 }
 
