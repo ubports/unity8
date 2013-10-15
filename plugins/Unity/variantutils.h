0e92f125bb2b6732129821aa06a34ef2fa13440a
Michal Hruby <michal.mhr@gmail.com>
2013-10-15 15:06:59 +0100

    Add unit tests

--- plugins/Unity/variantutils.h
+++ plugins/Unity/variantutils.h
@@ -29,6 +29,7 @@
 unity::glib::HintsMap convertToHintsMap(const QHash<QString, QVariant> &val);
 unity::glib::HintsMap convertToHintsMap(const QVariant &var);
 QVariantHash convertToQVariantHash(const unity::glib::HintsMap &var);
-QVariantHash subscopeUriToMetadataHash(const QString &metadata_string);
+/* used in tests, so we have to export it */
+Q_DECL_EXPORT QVariantHash subscopeUriToMetadataHash(const QString &metadata_string);
 
 #endif
