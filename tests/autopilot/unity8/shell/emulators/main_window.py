10f1878e6c4f60f9b0f7553dcd37070daf5fa646
Allan LeSage <allan.lesage@canonical.com>
2013-10-21 16:18:05 -0500

    Add emulators and stub tests for most indicators.

--- tests/autopilot/unity8/shell/emulators/main_window.py
+++ tests/autopilot/unity8/shell/emulators/main_window.py
@@ -93,3 +93,33 @@ class MainWindow(object):
 
     def get_pinentryField(self):
         return self.app.select_single(objectName="pinentryField")
+
+    def get_network_indicator(self):
+        return self.app.select_single(
+            "Tab",
+            objectName="indicator-network")
+
+    def get_location_indicator(self):
+        return self.app.select_single(
+            "Tab",
+            objectName="indicator-location")
+
+    def get_messaging_indicator(self):
+        return self.app.select_single(
+            "Tab",
+            objectName="indicator-messages")
+
+    def get_power_indicator(self):
+        return self.app.select_single(
+            "Tab",
+            objectName="indicator-power")
+
+    def get_sound_indicator(self):
+        return self.app.select_single(
+            "Tab",
+            objectName="indicator-sound")
+
+    def get_datetime_indicator(self):
+        return self.app.select_single(
+            "Tab",
+            objectName="indicator-datetime")
---
+++ tests/autopilot/unity8/shell/tests/test_location_indicator.py
@@ -0,0 +1,34 @@
+# -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
+#
+# Unity Autopilot Test Suite
+# Copyright (C) 2012-2013 Canonical
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from __future__ import absolute_import
+
+from unity8.shell.tests import UnityTestCase, _get_device_emulation_scenarios
+
+
+class TestLocationIndicator(UnityTestCase):
+
+    def test_location_indicator_exists(self):
+        """The 'indicator-location' tab can be found."""
+
+        self.launch_unity()
+        self.main_window.get_greeter().swipe()
+        window = self.main_window.get_qml_view()
+        location_indicator = self.main_window.get_location_indicator()
+        self.assertIsNotNone(location_indicator)
