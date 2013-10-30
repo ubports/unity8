cc828c3305f8d0c848a2da174411280af0d11b16
Albert Astals <albert.astals@canonical.com>
2013-10-30 10:22:13 +0100

    Fix header going bad when setContentHeight ends up moving the viewport
    How to reproduce the bug easily without the patch:
     * In the Dash Home, search for london
     * Scroll to the bottom
     * Start moving to the apps scope very slowly
     * At around 3/4 of the move you'll see the header in the home scope went to a bad position
     * Go back to the Dash Home

--- tests/plugins/ListViewWithPageHeader/listviewwithpageheadertest.cpp
+++ tests/plugins/ListViewWithPageHeader/listviewwithpageheadertest.cpp
@@ -1824,6 +1824,22 @@ private Q_SLOTS:
         lvwph->positionAtBeginning();
     }
 
+    void testHeaderPositionBug1240118()
+    {
+        scrollToBottom();
+        lvwph->showHeader();
+        QTRY_VERIFY(!lvwph->m_contentYAnimation->isRunning());
+        QTest::qWait(100);
+        QMetaObject::invokeMethod(model, "insertItem", Q_ARG(QVariant, 0), Q_ARG(QVariant, 100));
+        model->setProperty(3, "size", 10);
+        model->setProperty(4, "size", 10);
+        model->setProperty(5, "size", 10);
+        model->setProperty(6, "size", 10);
+        QTest::qWait(100);
+        QTRY_COMPARE(lvwph->m_minYExtent, 210.);
+        QTRY_COMPARE(lvwph->m_headerItem->y(), -lvwph->m_minYExtent);
+    }
+
 private:
     QQuickView *view;
     ListViewWithPageHeader *lvwph;
