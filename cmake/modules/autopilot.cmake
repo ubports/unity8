7eecbc65e69e39aeb7213664c10569df4a741d11
Didier Roche <didrocks@ubuntu.com>
2014-02-18 12:44:10 +0100

    switch the default theme to suru

--- cmake/modules/autopilot.cmake
+++ cmake/modules/autopilot.cmake
@@ -2,7 +2,7 @@ add_custom_target(autopilot)
 
 function(declare_autopilot_test TEST_NAME TEST_SUITE WORKING_DIR)
     add_custom_target(autopilot-${TEST_NAME}
-        COMMAND LANG=C UBUNTU_ICON_THEME=ubuntu-mobile QML2_IMPORT_PATH=${SHELL_INSTALL_QML}/mocks autopilot run ${TEST_SUITE}
+        COMMAND LANG=C UBUNTU_ICON_THEME=suru QML2_IMPORT_PATH=${SHELL_INSTALL_QML}/mocks autopilot run ${TEST_SUITE}
         WORKING_DIRECTORY ${WORKING_DIR}
         DEPENDS fake_install
     )
