00c0b4c9b8dc4825b0dec53260891d7c22ffb9ad
Nick Dedekind <nicholas.dedekind@gmail.com>
2014-05-08 11:33:05 +0100

    Added icon serialising test

--- tests/plugins/Unity/Indicators/rootactionstatetest.cpp
+++ tests/plugins/Unity/Indicators/rootactionstatetest.cpp
@@ -21,6 +21,7 @@
 
 #include <unitymenumodel.h>
 #include <QtTest>
+#include <gio/gio.h>
 
 class RootActionStateTest : public QObject
 {
@@ -51,6 +52,40 @@ private Q_SLOTS:
         delete menuModel;
         QVERIFY(rootState->menu() == NULL);
         delete rootState;
+    }
+
+    void testToQVariantIcons()
+    {
+        GVariantBuilder builderIcons;
+        g_variant_builder_init(&builderIcons, G_VARIANT_TYPE_ARRAY);
+
+        for (int i = 0; i < 3; i ++) {
+            GIcon* icon = NULL;
+            icon = g_icon_new_for_string (QString("testIcon%1").arg(i).toUtf8().constData(), NULL);
+
+            g_variant_builder_add_value(&builderIcons, g_icon_serialize (icon));
+            g_object_unref(icon);
+        }
+
+        GVariantBuilder builderParams;
+        g_variant_builder_init(&builderParams, G_VARIANT_TYPE_ARRAY);
+        GVariant* icons = g_variant_builder_end (&builderIcons);
+        g_variant_ref_sink (icons);
+        g_variant_builder_add (&builderParams, "{sv}", g_strdup ("icons"), icons, NULL);
+
+        GVariant* params = g_variant_builder_end (&builderParams);
+
+        RootActionState rootState;
+        QVariant result = rootState.toQVariant(params);
+        g_variant_unref(params);
+
+        QVariantMap paramResult = result.toMap();
+        QVERIFY(paramResult.contains("icons"));
+
+        QStringList serializedIcons = paramResult["icons"].toStringList();
+        QVERIFY(serializedIcons[0] == "image://theme/testIcon0");
+        QVERIFY(serializedIcons[1] == "image://theme/testIcon1");
+        QVERIFY(serializedIcons[2] == "image://theme/testIcon2");
     }
 };
 
