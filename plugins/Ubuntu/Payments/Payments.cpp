5d61a55485add80d910042145215d5c248c290e5
Rodney Dawes <rodney.dawes@canonical.com>
2014-06-25 18:05:11 -0400

    Remove an incorrect authored by line.

--- plugins/Ubuntu/Payments/Payments.cpp
+++ plugins/Ubuntu/Payments/Payments.cpp
@@ -14,8 +14,6 @@
  *
  * You should have received a copy of both the GNU Lesser General Public
  * License along with this program. If not, see <http://www.gnu.org/licenses/>
- *
- * Authored by: Diego Sarmentero <diego.sarmentero@canonical.com>
  */
 
 #include <QDebug>
