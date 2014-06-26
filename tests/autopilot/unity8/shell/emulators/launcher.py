18a1a17887ab9c27ceb060998f64fa8e63ce7c8f
Leo Arias <leo.arias@canonical.com>
2014-06-26 00:05:20 -0600

    Made the test pass.

--- tests/autopilot/unity8/shell/emulators/launcher.py
+++ tests/autopilot/unity8/shell/emulators/launcher.py
@@ -17,7 +17,12 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+import logging
+
 from unity8.shell.emulators import UnityEmulatorBase
+
+
+logger = logging.getLogger(__name__)
 
 
 class Launcher(UnityEmulatorBase):
@@ -26,10 +31,13 @@ class Launcher(UnityEmulatorBase):
 
     def show(self):
         """Swipes open the launcher."""
-        view = self.get_root_instance().select_single('QQuickView')
-        start_x = view.x + 1
-        start_y = view.y + view.height / 2
-        stop_x = start_x + self.panelWidth + 1
-        stop_y = start_y
-        self.pointing_device.drag(start_x, start_y, stop_x, stop_y)
-        self.shown.wait_for(True)
+        if not self.shown:
+            view = self.get_root_instance().select_single('QQuickView')
+            start_x = view.x + 1
+            start_y = view.y + view.height / 2
+            stop_x = start_x + self.panelWidth + 1
+            stop_y = start_y
+            self.pointing_device.drag(start_x, start_y, stop_x, stop_y)
+            self.shown.wait_for(True)
+        else:
+            logger.debug('The launcher is already opened.')
