4c4ef3816e1c15294752669541e0cf7fe80cf508
Leo Arias <leo.arias@canonical.com>
2014-06-26 00:47:51 -0600

    Typo

--- tests/autopilot/unity8/application_lifecycle/tests/test_application_lifecycle.py
+++ tests/autopilot/unity8/application_lifecycle/tests/test_application_lifecycle.py
@@ -118,7 +118,7 @@ class ApplicationLifecycleTests(tests.ApplicationLifeCycleTestCase):
 
     def test_click_dash_icon_must_unfocus_application(self):
         application_name = self.launch_fake_app()
-        self.assert_current_focuse_application(application_name)
+        self.assert_current_focused_application(application_name)
 
         launcher = self.main_window.open_launcher()
         import pdb; pdb.set_trace()
