6262458aa10dffb0cb840a1aa6caff92c52a40bd
Michael Terry <michael.terry@canonical.com>
2014-07-01 16:21:23 -0400

    Use PAM to check password validity

--- tests/mocks/LightDM/demo/GreeterPrivate.cpp
+++ tests/mocks/LightDM/demo/GreeterPrivate.cpp
@@ -18,64 +18,201 @@
 
 #include "../Greeter.h"
 #include "../GreeterPrivate.h"
-#include <QtCore/QDir>
-#include <QtCore/QProcess>
-#include <QtCore/QSettings>
+#include <QFuture>
+#include <QFutureInterface>
+#include <QFutureWatcher>
+#include <QQueue>
+#include <QtConcurrent>
+#include <QVector>
+#include <security/pam_appl.h>
 
 namespace QLightDM
 {
 
+class GreeterImpl : public QObject
+{
+    Q_OBJECT
+
+    typedef QFutureInterface<QString> ResponseFuture;
+
+public:
+    explicit GreeterImpl(Greeter *parent, GreeterPrivate *greeterPrivate)
+        : QObject(parent),
+          greeter(parent),
+          greeterPrivate(greeterPrivate),
+          pamHandle(NULL)
+    {
+        qRegisterMetaType<QLightDM::GreeterImpl::ResponseFuture>("QLightDM::GreeterImpl::ResponseFuture");
+
+        connect(&futureWatcher, SIGNAL(finished()),
+                this, SLOT(finishPam()));
+        connect(this, SIGNAL(showMessage(QString, QLightDM::Greeter::MessageType)),
+                greeter, SIGNAL(showMessage(QString, QLightDM::Greeter::MessageType)));
+        connect(this, SIGNAL(showPrompt(QString, QLightDM::Greeter::PromptType, QLightDM::GreeterImpl::ResponseFuture)),
+                this, SLOT(handlePrompt(QString, QLightDM::Greeter::PromptType, QLightDM::GreeterImpl::ResponseFuture)));
+    }
+
+    void start(QString username)
+    {
+        pam_conv conversation;
+        conversation.conv = converseWithPam;
+        conversation.appdata_ptr = static_cast<void*>(this);
+
+        if (pam_start("lightdm", username.toUtf8(),
+                       &conversation, &pamHandle) == PAM_SUCCESS) {
+            futureWatcher.setFuture(QtConcurrent::run(authenticateWithPam, pamHandle));
+        } else {
+            greeterPrivate->authenticated = false;
+            Q_EMIT greeter->authenticationComplete();
+        }
+    }
+
+    static int authenticateWithPam(pam_handle* pamHandle)
+    {
+        int pamStatus = pam_authenticate(pamHandle, 0);
+        if (pamStatus == PAM_SUCCESS)
+          pamStatus = pam_acct_mgmt(pamHandle, 0);
+        if (pamStatus == PAM_NEW_AUTHTOK_REQD)
+          pamStatus = pam_chauthtok(pamHandle, PAM_CHANGE_EXPIRED_AUTHTOK);
+        if (pamStatus == PAM_SUCCESS)
+          pam_setcred(pamHandle, PAM_REINITIALIZE_CRED);
+        return pamStatus;
+    }
+
+    static int converseWithPam(int num_msg, const pam_message** msg,
+                               pam_response** resp, void* appdata_ptr)
+    {
+        if (num_msg <= 0)
+            return PAM_CONV_ERR;
+
+        auto* tmp_response = static_cast<pam_response*>(calloc(num_msg, sizeof(pam_response)));
+        if (!tmp_response)
+            return PAM_CONV_ERR;
+
+        GreeterImpl* impl = static_cast<GreeterImpl*>(appdata_ptr);
+
+        bool raise_error = false;
+        int count;
+        QVector<ResponseFuture> responses;
+
+        for (count = 0; count < num_msg && !raise_error; ++count)
+        {
+            switch (msg[count]->msg_style)
+            {
+            case PAM_PROMPT_ECHO_ON:
+            {
+                QString message(msg[count]->msg);
+                responses.append(ResponseFuture());
+                responses.last().reportStarted();
+                Q_EMIT impl->showPrompt(message, Greeter::PromptTypeQuestion, responses.last());
+                break;
+            }
+            case PAM_PROMPT_ECHO_OFF:
+            {
+                QString message(msg[count]->msg);
+                responses.append(ResponseFuture());
+                responses.last().reportStarted();
+                Q_EMIT impl->showPrompt(message, Greeter::PromptTypeSecret, responses.last());
+            }
+            case PAM_TEXT_INFO:
+            {
+                QString message(msg[count]->msg);
+                Q_EMIT impl->showMessage(message, Greeter::MessageTypeInfo);
+            }
+            default:
+            {
+                QString message(msg[count]->msg);
+                Q_EMIT impl->showMessage(message, Greeter::MessageTypeError);
+            }
+            }
+        }
+
+        int i = 0;
+
+        for (auto &response : responses)
+        {
+            pam_response* resp_item = &tmp_response[i++];
+            resp_item->resp_retcode = 0;
+            resp_item->resp = strdup(response.future().result().toUtf8());
+
+            if (!resp_item->resp)
+            {
+                raise_error = true;
+                break;
+            }
+        }
+
+        if (raise_error)
+        {
+            for (int i = 0; i < count; ++i)
+                free(tmp_response[i].resp);
+
+            free(tmp_response);
+            return PAM_CONV_ERR;
+        }
+        else
+        {
+            *resp = tmp_response;
+            return PAM_SUCCESS;
+        }
+    }
+
+public Q_SLOTS:
+    void respond(QString response)
+    {
+        if (!futures.isEmpty()) {
+            futures.dequeue().reportFinished(&response);
+        }
+    }
+
+Q_SIGNALS:
+    void showMessage(QString text, QLightDM::Greeter::MessageType type);
+    void showPrompt(QString text, QLightDM::Greeter::PromptType type, QLightDM::GreeterImpl::ResponseFuture response);
+
+private Q_SLOTS:
+    void finishPam()
+    {
+        int pamStatus = futureWatcher.result();
+
+        pam_end(pamHandle, pamStatus);
+        pamHandle = NULL;
+
+        greeterPrivate->authenticated = (pamStatus == PAM_SUCCESS);
+        Q_EMIT greeter->authenticationComplete();
+    }
+
+    void handlePrompt(QString text, QLightDM::Greeter::PromptType type, QLightDM::GreeterImpl::ResponseFuture future)
+    {
+        futures.enqueue(future);
+        Q_EMIT greeter->showPrompt(text, type);
+    }
+
+private:
+    Greeter *greeter;
+    GreeterPrivate *greeterPrivate;
+    pam_handle* pamHandle;
+    QFutureWatcher<int> futureWatcher;
+    QQueue<ResponseFuture> futures;
+};
+
 GreeterPrivate::GreeterPrivate(Greeter* parent)
   : authenticated(false),
     authenticationUser(),
+    m_impl(new GreeterImpl(parent, this)),
     q_ptr(parent)
 {
 }
 
 void GreeterPrivate::handleAuthenticate()
 {
-    Q_Q(Greeter);
-
-    QSettings settings(QDir::homePath() + "/.unity8-greeter-demo", QSettings::NativeFormat);
-    settings.beginGroup(authenticationUser);
-    QVariant password = settings.value("password", "none");
-
-    if (password == "pin") {
-        Q_EMIT q->showPrompt("PIN", Greeter::PromptTypeSecret);
-    } else if (password == "keyboard") {
-        Q_EMIT q->showPrompt("Password", Greeter::PromptTypeSecret);
-    } else {
-        authenticated = true;
-        Q_EMIT q->authenticationComplete();
-    }
+    m_impl->start(authenticationUser);
 }
 
 void GreeterPrivate::handleRespond(const QString &response)
 {
-    Q_Q(Greeter);
-
-    QSettings settings(QDir::homePath() + "/.unity8-greeter-demo", QSettings::NativeFormat);
-    settings.beginGroup(authenticationUser);
-    QVariant passwordValue(settings.value("passwordValue", QString()));
-    QStringList passwordParts = passwordValue.toString().split('$', QString::SkipEmptyParts);
-
-    // We only support passwd type 6 (sha512) for now
-    if (passwordParts.length() == 3 && passwordParts[0] == "6") {
-        QString command = "mkpasswd --method=sha-512 --stdin --salt=" + passwordParts[1];
-
-        QProcess process;
-        process.start(command);
-        process.write(response.toLatin1());
-        process.closeWriteChannel();
-        process.waitForFinished();
-
-        QString result = QString(process.readAllStandardOutput()).trimmed();
-        authenticated = result == passwordValue;
-    } else {
-        authenticated = false;
-    }
-
-    Q_EMIT q->authenticationComplete();
+    m_impl->respond(response);
 }
 
 }
+
+#include "GreeterPrivate.moc"
