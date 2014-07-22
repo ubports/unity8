6ab6bce8938fea21f4205ac35bf8c7e57bbaad55
Michael Zanetti <michael.zanetti@canonical.com>
2014-07-22 16:04:22 +0200

    reverting r 1076

--- tests/mocks/QMenuModel/unitymenumodel.cpp
+++ tests/mocks/QMenuModel/unitymenumodel.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013-2014 Canonical, Ltd.
+ * Copyright (C) 2013 Canonical, Ltd.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,7 +14,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * Authors: Nick Dedekind <nick.dedekind@canonical.com>
- *          Daniel d'Andrada <daniel.dandrada@canonical.com>
  */
 
 #include "unitymenumodel.h"
@@ -240,9 +239,4 @@ void UnityMenuModel::registerAction(UnityMenuAction*)
 
 void UnityMenuModel::unregisterAction(UnityMenuAction*)
 {
-}
-
-QByteArray UnityMenuModel::nameOwner() const
-{
-    return QByteArray();
 }
