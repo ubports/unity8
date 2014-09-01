aa89d2765ee045b5d5f1f5b91a6ffb9afbad4461
Albert Astals <albert.astals@canonical.com>
2014-09-01 12:45:25 +0200

    make list non interactive while on header animation

--- plugins/Dash/listviewwithpageheader.h
+++ plugins/Dash/listviewwithpageheader.h
@@ -118,7 +118,7 @@ private Q_SLOTS:
     void onContentWidthChanged();
     void onHeightChanged();
     void onModelUpdated(const QQmlChangeSet &changeSet, bool reset);
-    void onShowHeaderAnimationFinished();
+    void contentYAnimationRunningChanged(bool running);
 
 private:
     class ListItem
