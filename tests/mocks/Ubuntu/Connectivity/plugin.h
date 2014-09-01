6a99ed94ac296aae91370e3d929452c7b406494d
Albert Astals <albert.astals@canonical.com>
2014-09-01 11:13:08 +0200

    Add Ubuntu.Connectivity mock

---
+++ tests/mocks/Ubuntu/Connectivity/plugin.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright © 2014 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *     Antti Kaijanmäki <antti.kaijanmaki@canonical.com>
+ */
+
+#ifndef QML_CONNECTIVITY_PLUGIN_H
+#define QML_CONNECTIVITY_PLUGIN_H
+
+#include <QQmlExtensionPlugin>
+
+class QmlConnectivityNetworkingPlugin : public QQmlExtensionPlugin {
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID "org.qt-project.Qt.QQmlExtensionInterface")
+    Q_INTERFACES(QQmlExtensionInterface)
+
+public:
+    void registerTypes(const char *uri);
+    void initializeEngine(QQmlEngine *engine, const char *uri);
+};
+
+#endif // QML_CONNECTIVITY_PLUGIN_H
+
