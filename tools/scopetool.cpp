ca32b97222d59176fc996256ef01dad3b6c0135b
Pawel Stolowski <pawel.stolowski@canonical.com>
2014-09-04 13:36:55 +0200

    Set UNITY_SCOPES_NO_FAVORITES env var.

--- tools/scopetool.cpp
+++ tools/scopetool.cpp
@@ -44,6 +44,9 @@ int main(int argc, char *argv[])
     setenv("QML_FORCE_THREADED_RENDERER", "1", 1);
     setenv("QML_FIXED_ANIMATION_STEP", "1", 1);
 
+    // ignore favorites in unity-scopes-shell plugin
+    setenv("UNITY_SCOPES_NO_FAVORITES", "1", 1);
+
     QGuiApplication::setApplicationName("Unity Scope Tool");
     QGuiApplication *application;
     application = new QGuiApplication(argc, argv);
