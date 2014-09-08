f98678d0c5de2337f6c02374a6ea50ad082f8028
Michał Sawicz <michal.sawicz@canonical.com>
2014-09-08 11:28:53 +0200

    Rework run_on_device.sh to support non-root adb shell

--- run_on_device.sh
+++ run_on_device.sh
@@ -4,8 +4,8 @@ USER=phablet
 USER_ID=32011
 PACKAGE=unity8
 BINARY=unity8
-TARGET_IP=${TARGET_IP-127.0.0.1}
-TARGET_SSH_PORT=${TARGET_SSH_PORT-2222}
+TARGET_IP=127.0.0.1
+TARGET_SSH_PORT=2222
 TARGET_DEBUG_PORT=3768
 RUN_OPTIONS=-qmljsdebugger=port:$TARGET_DEBUG_PORT
 SETUP=false
@@ -14,17 +14,22 @@ PINLOCK=false
 KEYLOCK=false
 NUM_JOBS='$(( `grep -c ^processor /proc/cpuinfo` + 1 ))'
 FLIPPED=false
-CHROOT_PREFIX="/data/ubuntu"
 SSH_WAS_STARTED=0
 SSH_STARTED=0
+PASSWORD=""
+PASSFILE=""
 
 usage() {
-    echo "usage: run_on_device [OPTIONS]\n"
-    echo "Script to setup a build environment for the shell and sync build and run it on the device\n"
+    echo "usage: run_on_device -a SUDO_PASSWORD [OPTIONS]"
+    echo
+    echo "Script to setup a build environment for the shell and sync build and run it on the device"
+    echo
     echo "OPTIONS:"
-    echo "  -s, --setup   Setup the build environment"
-    echo "  -p, --pinlock Enable a PIN lock screen when running"
-    echo "  -k, --keylock Enable a Keyboard lock screen when running"
+    echo "  -a The sudo password to use"
+    echo "  -s Setup the build environment"
+    echo "  -g Run with gdb"
+    echo "  -p Enable a PIN lock screen when running"
+    echo "  -k Enable a Keyboard lock screen when running"
     echo ""
     echo "IMPORTANT:"
     echo " * Make sure to have networking setup on the device beforehand."
@@ -34,7 +39,7 @@ usage() {
 
 start_ssh() {
     if [ $SSH_STARTED -eq 0 ]; then
-        exec_with_adb initctl start ssh
+        adb_sudo initctl start ssh
         SSH_STARTED=1
     fi
 }
@@ -44,25 +49,13 @@ exec_with_ssh() {
     ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -t $USER@$TARGET_IP -p $TARGET_SSH_PORT sudo -u $USER -i bash -ic \"$@\"
 }
 
-exec_with_adb() {
-    if $FLIPPED; then
-        adb shell $@
-    else
-        adb shell chroot /data/ubuntu /usr/bin/env -i PATH=/bin:/usr/bin:/sbin:/usr/sbin:/tools/bin "$@"
-    fi
-}
-
-adb_root() {
-    adb root
-    adb wait-for-device
+adb_sudo() {
+    adb shell "SUDO_ASKPASS=${PASSFILE} sudo --askpass $@"
 }
 
 install_ssh_key() {
-    ssh-keygen -R $TARGET_IP
-    HOME_DIR="${CHROOT_PREFIX}/home/phablet"
+    HOME_DIR="/home/phablet"
     adb push ~/.ssh/id_rsa.pub $HOME_DIR/.ssh/authorized_keys
-    adb shell chown $USER_ID:$USER_ID $HOME_DIR/.ssh
-    adb shell chown $USER_ID:$USER_ID $HOME_DIR/.ssh/authorized_keys
     adb shell chmod 700 $HOME_DIR/.ssh
     adb shell chmod 600 $HOME_DIR/.ssh/authorized_keys
 }
@@ -73,8 +66,8 @@ setup_adb_forwarding() {
 }
 
 install_dependencies() {
-    exec_with_adb apt-get update
-    exec_with_adb apt-get -y --force-yes install build-essential rsync bzr ccache gdb ninja-build devscripts equivs unity-plugin-scopes
+    adb_sudo apt-get update
+    adb_sudo apt-get -y --force-yes install build-essential rsync bzr ccache gdb ninja-build devscripts equivs unity-plugin-scopes
 }
 
 sync_code() {
@@ -108,37 +101,35 @@ run() {
     exec_with_ssh "start unity8"
 }
 
-set -- `getopt -n$0 -u -a --longoptions="setup,gdb,pinlock,keylock,help" "sgpkh" "$@"`
-
-# FIXME: giving incorrect arguments does not call usage and exit
-while [ $# -gt 0 ]
-do
-    case "$1" in
-       -s|--setup)   SETUP=true;;
-       -g|--gdb)     GDB=true;;
-       -p|--pinlock)     PINLOCK=true;;
-       -k|--keylock)     KEYLOCK=true;;
-       -h|--help)    usage;;
-       --)           shift;break;;
+while getopts "a:sgpkh" OPTION; do
+    case "$OPTION" in
+       a|password) PASSWORD=$OPTARG;;
+       s|setup)   SETUP=true;;
+       g|gdb)     GDB=true;;
+       p|pinlock)     PINLOCK=true;;
+       k|keylock)     KEYLOCK=true;;
+       h|help)    usage;;
+       ?)         echo;usage;;
     esac
-    shift
 done
 
+shift $((OPTIND - 1))
 
-adb_root
-[ "${TARGET_IP}" = "127.0.0.1" ] && setup_adb_forwarding
-
-if [ "`adb shell 'grep -q Ubuntu /etc/lsb-release 2> /dev/null && echo -n FLIPPED'`" = "FLIPPED" ]; then
-    FLIPPED=true
-    CHROOT_PREFIX=""
-    if [ "${TARGET_IP}" != "127.0.0.1" ]; then
-        echo "ERROR: Flipped image detected, adb over TCP/IP isn't supported, yet :/"
-        echo "Unset TARGET_IP and try again."
-        exit 2
-    fi
+if [ -z "${PASSWORD}" ]; then
+    echo "ERROR: You need to provide a sudo password..."
+    echo
+    usage
 fi
 
-status_output=$(exec_with_adb initctl status ssh)
+PASSFILE=$( adb shell "FILE=\`mktemp\`
+                       echo \"#!/bin/sh\" > \$FILE
+                       echo \"echo \\\"${PASSWORD}\\\"\" >> \$FILE
+                       chmod +x \$FILE
+                       echo \$FILE" | tr -d '\r' )
+
+setup_adb_forwarding
+
+status_output=$(adb_sudo initctl status ssh)
 if [[ $status_output == "ssh start/running, process "* ]]; then
     SSH_WAS_STARTED=1
 fi
@@ -158,6 +149,8 @@ else
     run
 fi
 
+adb shell "rm ${PASSFILE}"
+
 if [ $SSH_WAS_STARTED -eq 0 ]; then
-    exec_with_adb initctl stop ssh
+    adb_sudo initctl stop ssh
 fi
