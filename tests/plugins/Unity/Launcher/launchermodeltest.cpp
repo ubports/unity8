294d9b76dde0d418157eeaae1d14b83480e2f4f0
Michael Zanetti <michael.zanetti@canonical.com>
2014-09-29 16:26:56 +0200

    update the launcher tests mock appman backend

--- tests/plugins/Unity/Launcher/launchermodeltest.cpp
+++ tests/plugins/Unity/Launcher/launchermodeltest.cpp
@@ -105,8 +105,8 @@ public:
     bool requestFocusApplication(const QString &appId) { Q_UNUSED(appId); return true; }
     bool suspended() const { return false; }
     void setSuspended(bool) {}
-    bool dashActive() const { return false; }
-    void setDashActive(bool) {}
+    bool forceDashActive() const { return false; }
+    void setForceDashActive(bool) {}
 
 private:
     QList<MockApp*> m_list;
