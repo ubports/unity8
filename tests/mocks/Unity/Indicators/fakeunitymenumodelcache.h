87e943f1069220c00c91c19a6df3a0c18d026efb
Nick Dedekind <nick.dedekind@canonical.com>
2014-10-07 11:53:18 +0100

    copywrite update

--- tests/mocks/Unity/Indicators/fakeunitymenumodelcache.h
+++ tests/mocks/Unity/Indicators/fakeunitymenumodelcache.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 Canonical Ltd.
+ * Copyright 2014 Canonical Ltd.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -12,9 +12,6 @@
  *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Authors:
- *      Nick Dedekind <nick.dedekind@canonical.com>
  */
 
 #ifndef FAKEUNITYMENUMODELCACHE_H
