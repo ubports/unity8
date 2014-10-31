2664a82af50d37dbd8820b0ad414a46ea30277ef
Albert Astals <albert.astals@canonical.com>
2014-10-31 15:20:45 +0100

    Make sure we don't divide by 0

--- plugins/Dash/croppedimagesizer.cpp
+++ plugins/Dash/croppedimagesizer.cpp
@@ -97,12 +97,14 @@ void CroppedImageSizer::requestFinished()
     QImageReader m_reader(m_reply);
     const QSize imageSize = m_reader.size();
     if (imageSize.isValid()) {
-        const qreal ar = width() / height();
-        const qreal ssar = imageSize.width() / imageSize.height();
-        if (ar > ssar) {
-            m_sourceSize = QSize(m_width, 0);
-        } else {
-            m_sourceSize = QSize(0, m_height);
+        if (m_height > 0 && imageSize.height() > 0) {
+            const qreal ar = m_width / m_height;
+            const qreal ssar = imageSize.width() / imageSize.height();
+            if (ar > ssar) {
+                m_sourceSize = QSize(m_width, 0);
+            } else {
+                m_sourceSize = QSize(0, m_height);
+            }
         }
     } else {
         qWarning() << "Could not find size of" << m_source;
