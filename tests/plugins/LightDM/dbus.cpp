cb21dfaebc8135749e31fa316524ae9bcf8e6c50
Michael Terry <michael.terry@canonical.com>
2014-12-11 10:48:35 -0500

    Clean up greeter dbus test

--- tests/plugins/LightDM/dbus.cpp
+++ tests/plugins/LightDM/dbus.cpp
@@ -159,9 +159,12 @@ private Q_SLOTS:
     {
         QSignalSpy spy(this, SIGNAL(PropertiesChangedRelay(QString, QVariantMap, QStringList)));
         greeter->authenticate("no-password");
-        spy.wait();
 
-        QCOMPARE(spy.count(), 2); // once for locked, once for user; first will be locked mode
+        // once for locked, once for user; first will be locked mode
+        while (spy.count() != 2) {
+            spy.wait();
+        }
+
         QList<QVariant> arguments = spy.takeFirst();
         QVERIFY(arguments.at(0).toString() == "com.canonical.UnityGreeter.List");
         QVERIFY(arguments.at(1).toMap().contains("EntryIsLocked"));
