a86209fe52308657b6f0b78bcace52054dd80f0c
Albert Astals <albert.astals@canonical.com>
2014-12-16 10:03:37 +0100

    Manage Dash: Ellide properly

--- tests/mocks/Unity/fake_scopes.cpp
+++ tests/mocks/Unity/fake_scopes.cpp
@@ -52,7 +52,7 @@ void Scopes::updateScopes()
     addScope(new Scope("MockScope1", "People", true, this));
     addScope(new Scope("MockScope2", "Music", false, this));
     addScope(new Scope("clickscope", "Apps", true, this));
-    addScope(new Scope("MockScope5", "Videos", true, this));
+    addScope(new Scope("MockScope5", "Videos this is long ab cd ef gh ij kl", true, this));
     addScope(new Scope("SingleCategoryScope", "Single", true, this, 1));
     addScope(new Scope("MockScope4", "MS4", true, this));
     addScope(new Scope("MockScope6", "MS6", true, this));
