3d086c9825e30af2ea466445861a81a6a9c48f60
Ricardo Mendoza <ricardo.mendoza@canonical.com>
2015-01-23 00:23:03 +0100

    Improve code

--- tests/autopilot/unity8/sensors.py
+++ tests/autopilot/unity8/sensors.py
@@ -19,72 +19,69 @@
 
 """Control module for event injection to the fake/test platform sensors."""
 
-import os
-import sys
+from unity8 import process_helpers
+
 
 class FakePlatformSensors:
 
-    def __init__(self, pid):
-        self.pid = pid
-        try:
-            self.fifo = open("/tmp/sensor-fifo-%d" % self.pid, "w")
-        except IOError:
-            raise RuntimeError("Failed to open fifo /tmp/sensor-fifo-%d" % self.pid)
+    def __init__(self, pid=None):
+        self.pid = pid or process_helpers.get_unity_pid()
 
-    def __del__(self):
-        close(self.fifo)
-        
     def set_orientation_top_up(self):
-        self.fifo.write("70 accel -10.050858 -0.598550 0.756568\n")
-        self.fifo.write("70 accel -9.797073 -0.555455 1.019930\n")
-        self.fifo.write("70 accel -10.141838 -0.770933 0.632069\n")
-        self.fifo.write("70 accel -12.057199 -1.259350 1.690306\n")
-        self.fifo.write("70 accel -19.282900 -3.926491 3.098097\n")
-        self.fifo.write("70 accel -14.480132 -14.269443 1.216254\n")
-        self.fifo.write("70 accel 16.419436 4.242526 -7.714118\n")
-        self.fifo.write("70 accel 5.583278 8.279149 -1.848324\n")
-        self.fifo.write("70 accel 1.422156 8.547300 0.416591\n")
-        self.fifo.write("70 accel 4.357447 9.988609 -0.110133\n")
-        self.fifo.write("70 accel 0.699107 9.840169 0.756568\n")
-        self.fifo.write("70 accel 1.364695 9.844957 -0.287304\n")
-        self.fifo.flush()
+        with open("/tmp/sensor-fifo-{0}".format(self.pid), "w") as fifo:
+            fifo.write("70 accel -10.050858 -0.598550 0.756568\n")
+            fifo.write("70 accel -9.797073 -0.555455 1.019930\n")
+            fifo.write("70 accel -10.141838 -0.770933 0.632069\n")
+            fifo.write("70 accel -12.057199 -1.259350 1.690306\n")
+            fifo.write("70 accel -19.282900 -3.926491 3.098097\n")
+            fifo.write("70 accel -14.480132 -14.269443 1.216254\n")
+            fifo.write("70 accel 16.419436 4.242526 -7.714118\n")
+            fifo.write("70 accel 5.583278 8.279149 -1.848324\n")
+            fifo.write("70 accel 1.422156 8.547300 0.416591\n")
+            fifo.write("70 accel 4.357447 9.988609 -0.110133\n")
+            fifo.write("70 accel 0.699107 9.840169 0.756568\n")
+            fifo.write("70 accel 1.364695 9.844957 -0.287304\n")
+            fifo.flush()
 
     def set_orientation_top_down(self):
-        self.fifo.write("70 accel 9.538500 -0.603339 1.292869\n")
-        self.fifo.write("70 accel 9.485827 -0.636858 1.422156\n")
-        self.fifo.write("70 accel 9.677363 -0.402226 1.374272\n")
-        self.fifo.write("70 accel 9.303867 -0.507571 1.283292\n")
-        self.fifo.write("70 accel 8.604761 -1.015141 1.436521\n")
-        self.fifo.write("70 accel 7.580042 -2.001553 0.521936\n")
-        self.fifo.write("70 accel 7.503428 -4.247314 0.502782\n")
-        self.fifo.write("70 accel 7.067683 -7.240066 0.842759\n")
-        self.fifo.write("70 accel 6.488286 -9.873688 -0.541090\n")
-        self.fifo.write("70 accel 6.229713 -9.241618 -1.048660\n")
-        self.fifo.write("70 accel 4.046201 -9.198523 -0.057461\n")
-        self.fifo.write("70 accel 2.398990 -9.629479 0.957681\n")
-        self.fifo.write("70 accel 1.632846 -9.361329 -0.311246\n")
-        self.fifo.write("70 accel -0.181959 -9.696517 -0.301669\n")
-        self.fifo.flush()
+        with open("/tmp/sensor-fifo-%d" % self.pid, "w") as fifo:
+            fifo.write("70 accel 9.538500 -0.603339 1.292869\n")
+            fifo.write("70 accel 9.485827 -0.636858 1.422156\n")
+            fifo.write("70 accel 9.677363 -0.402226 1.374272\n")
+            fifo.write("70 accel 9.303867 -0.507571 1.283292\n")
+            fifo.write("70 accel 8.604761 -1.015141 1.436521\n")
+            fifo.write("70 accel 7.580042 -2.001553 0.521936\n")
+            fifo.write("70 accel 7.503428 -4.247314 0.502782\n")
+            fifo.write("70 accel 7.067683 -7.240066 0.842759\n")
+            fifo.write("70 accel 6.488286 -9.873688 -0.541090\n")
+            fifo.write("70 accel 6.229713 -9.241618 -1.048660\n")
+            fifo.write("70 accel 4.046201 -9.198523 -0.057461\n")
+            fifo.write("70 accel 2.398990 -9.629479 0.957681\n")
+            fifo.write("70 accel 1.632846 -9.361329 -0.311246\n")
+            fifo.write("70 accel -0.181959 -9.696517 -0.301669\n")
+            fifo.flush()
 
     def set_orientation_left_up(self):
-        self.fifo.write("70 accel 0.196325 9.878476 0.948104\n")
-        self.fifo.write("70 accel 0.258574 9.955091 1.091756\n")
-        self.fifo.write("70 accel 0.287304 10.041282 1.134852\n")
-        self.fifo.write("70 accel 1.537078 10.553641 1.561020\n")
-        self.fifo.write("70 accel 8.130709 10.093954 2.561796\n")
-        self.fifo.write("70 accel -0.229843 5.348647 1.723825\n")
-        self.fifo.write("70 accel -9.916783 0.488417 -3.418920\n")
-        self.fifo.write("70 accel -13.417107 -0.416591 -2.360683\n")
-        self.fifo.write("70 accel -13.872005 -2.049437 -0.574608\n")
-        self.fifo.flush()
+        with open("/tmp/sensor-fifo-%d" % self.pid, "w") as fifo:
+            fifo.write("70 accel 0.196325 9.878476 0.948104\n")
+            fifo.write("70 accel 0.258574 9.955091 1.091756\n")
+            fifo.write("70 accel 0.287304 10.041282 1.134852\n")
+            fifo.write("70 accel 1.537078 10.553641 1.561020\n")
+            fifo.write("70 accel 8.130709 10.093954 2.561796\n")
+            fifo.write("70 accel -0.229843 5.348647 1.723825\n")
+            fifo.write("70 accel -9.916783 0.488417 -3.418920\n")
+            fifo.write("70 accel -13.417107 -0.416591 -2.360683\n")
+            fifo.write("70 accel -13.872005 -2.049437 -0.574608\n")
+            fifo.flush()
 
     def set_orientation_right_up(self):
-        self.fifo.write("70 accel -0.799663 9.988609 1.197101\n")
-        self.fifo.write("70 accel -0.861913 9.864111 0.866701\n")
-        self.fifo.write("70 accel -1.776498 9.830592 1.273715\n")
-        self.fifo.write("70 accel -13.158532 2.217031 1.091756\n")
-        self.fifo.write("70 accel 5.056554 1.067814 0.799663\n")
-        self.fifo.write("70 accel 14.882358 2.896984 1.221043\n")
-        self.fifo.write("70 accel 9.466674 -0.363919 -1.029507\n")
-        self.fifo.write("70 accel 12.253524 -0.186748 -0.311246\n")
-        self.fifo.flush()
+        with open("/tmp/sensor-fifo-%d" % self.pid, "w") as fifo:
+            fifo.write("70 accel -0.799663 9.988609 1.197101\n")
+            fifo.write("70 accel -0.861913 9.864111 0.866701\n")
+            fifo.write("70 accel -1.776498 9.830592 1.273715\n")
+            fifo.write("70 accel -13.158532 2.217031 1.091756\n")
+            fifo.write("70 accel 5.056554 1.067814 0.799663\n")
+            fifo.write("70 accel 14.882358 2.896984 1.221043\n")
+            fifo.write("70 accel 9.466674 -0.363919 -1.029507\n")
+            fifo.write("70 accel 12.253524 -0.186748 -0.311246\n")
+            fifo.flush()
