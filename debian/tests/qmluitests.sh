9c24f6da72f53ef16ee2b8d33c07ef4d4e260cce
Michał Sawicz <michal.sawicz@canonical.com>
2015-04-30 22:44:37 +0200

    Add autopkgtest definition and adapt unity8_ test macros

---
+++ debian/tests/qmluitests.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+SHELL_QML_PATH=$(pkg-config --variable=plugindir unity-shell-api)
+
+dh_auto_configure -- -DCMAKE_INSTALL_LOCALSTATEDIR="/var" \
+                     -DARTIFACTS_DIR=${ADT_ARTIFACTS} \
+                     -DUNITY_PLUGINPATH=${SHELL_QML_PATH} \
+                     -DUNITY_MOCKPATH=${SHELL_QML_PATH}/mocks \
+                     -DQUIET_LOGGER=ON
+dh_auto_build -- -C tests/mocks 2>&1
+dh_auto_build -- -C tests/plugins 2>&1
+dh_auto_build -- -C tests/qmltests 2>&1
+dh_auto_build -- -C tests/uqmlscene 2>&1
+dh_auto_build -- -C tests/utils 2>&1
+dh_auto_build -- xvfbuitests
