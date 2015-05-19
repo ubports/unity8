928c6153773decc329c404196fb00b2ab3d5730f
Josh Arenson <joshua.arenson@canonical.com>
2015-05-19 13:57:42 -0500

    Add license

--- tests/autopilot/unity8/settings_wizard/tests/test_settings_wizard.py
+++ tests/autopilot/unity8/settings_wizard/tests/test_settings_wizard.py
@@ -1,3 +1,22 @@
+# -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
+#
+# Unity Autopilot Test Suite
+# Copyright (C) 2015 Canonical
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 from unity8.settings_wizard import fixture_setup
 from unity8.settings_wizard.emulators.settings_wizard import Wizard
 from unity8.shell import tests
