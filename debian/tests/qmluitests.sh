bbebf96ba1894ef7d90d4173afb8abfa9dde398a
Albert Astals Cid <albert.astals@canonical.com>
2015-07-14 09:40:47 +0200

    Don't make it quiet, it's harder to debug failures

--- debian/tests/qmluitests.sh
+++ debian/tests/qmluitests.sh
@@ -5,8 +5,7 @@ SHELL_QML_PATH=$(pkg-config --variable=plugindir unity-shell-api)
 dh_auto_configure -- -DCMAKE_INSTALL_LOCALSTATEDIR="/var" \
                      -DARTIFACTS_DIR=${ADT_ARTIFACTS} \
                      -DUNITY_PLUGINPATH=${SHELL_QML_PATH} \
-                     -DUNITY_MOCKPATH=${SHELL_QML_PATH}/mocks \
-                     -DQUIET_LOGGER=ON
+                     -DUNITY_MOCKPATH=${SHELL_QML_PATH}/mocks
 dh_auto_build --parallel -- -C tests/mocks
 dh_auto_build --parallel -- -C tests/plugins
 dh_auto_build --parallel -- -C tests/qmltests
