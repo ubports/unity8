db5f575c57fa16231326488af80a575d14d18eba
Lukáš Tinkl <lukas.tinkl@canonical.com>
2015-12-22 14:28:38 +0100

    un-comment locale related bits to restart
    verified it doesn't freeze/crash anymore

--- plugins/Wizard/System.cpp
+++ plugins/Wizard/System.cpp
@@ -101,10 +101,10 @@ void System::updateSessionLocale(const QString &locale)
     QLocale::setDefault(QLocale(locale));
 
     // Restart bits of the session to pick up new language.
-//    QProcess::startDetached(QStringLiteral("sh -c \"initctl emit indicator-services-end; \
-//                                     initctl stop scope-registry; \
-//                                     initctl stop smart-scopes-proxy; \
-//                                     initctl emit --no-wait indicator-services-start; \
-//                                     initctl restart --no-wait maliit-server; \
-//                                     initctl restart --no-wait unity8-dash\""));
+    QProcess::startDetached(QStringLiteral("sh -c \"initctl emit indicator-services-end; \
+                                     initctl stop scope-registry; \
+                                     initctl stop smart-scopes-proxy; \
+                                     initctl emit --no-wait indicator-services-start; \
+                                     initctl restart --no-wait maliit-server; \
+                                     initctl restart --no-wait unity8-dash\""));
 }
