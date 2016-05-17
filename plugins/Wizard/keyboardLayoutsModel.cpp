eb5ccd8f8b777b23e0d2b55f355ed2e421f8f3cd
Lukáš Tinkl <lukas.tinkl@canonical.com>
2016-05-17 18:29:48 +0200

    WIP keyboard layouts model

---
+++ plugins/Wizard/keyboardLayoutsModel.cpp
@@ -0,0 +1,165 @@
+/*
+ * Copyright (C) 2016 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3, as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranties of
+ * MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <QDBusMetaType>
+#include <QDebug>
+
+#include "keyboardLayoutsModel.h"
+
+typedef QList<QMap<QString, QString>> StringMapList;
+Q_DECLARE_METATYPE(StringMapList)
+
+struct KeyboardLayoutInfo {
+    QString id;
+    QString displayName;
+    QString shortName; // language
+    QString layoutAndVariant;
+};
+
+KeyboardLayoutsModel::KeyboardLayoutsModel(QObject *parent)
+    : QAbstractListModel(parent)
+{
+    m_roleNames = {
+        {LayoutIdRole, "layoutId"},
+        {DisplayNameRole, "displayName"},
+        {ShortNameRole, "shortName"},
+        {LayoutAndVariantRole, "layoutAndVariant"}
+    };
+
+    qDBusRegisterMetaType<StringMapList>();
+    m_xkbInfo = gnome_xkb_info_new();
+
+    connect(this, &KeyboardLayoutsModel::languageChanged, this, &KeyboardLayoutsModel::buildModel);
+    buildModel();
+}
+
+KeyboardLayoutsModel::~KeyboardLayoutsModel()
+{
+    if (m_xkbInfo != nullptr) {
+        g_object_unref(m_xkbInfo);
+    }
+}
+
+QString KeyboardLayoutsModel::language() const
+{
+    return m_language;
+}
+
+void KeyboardLayoutsModel::setLanguage(const QString &language)
+{
+    if (m_language == language)
+        return;
+
+    m_language = language;
+    Q_EMIT languageChanged(language);
+}
+
+static bool compareLayouts(const KeyboardLayoutInfo &layout0, const KeyboardLayoutInfo &layout1)
+{
+    QString name0(layout0.displayName);
+    QString name1(layout1.displayName);
+
+    if (name0 == name1) {
+        name0 = layout0.shortName;
+        name1 = layout1.shortName;
+
+        if (name0 == name1) {
+            name0 = layout0.id;
+            name1 = layout1.id;
+        }
+    }
+
+    return QString::localeAwareCompare(name0, name1) < 0;
+}
+
+void KeyboardLayoutsModel::buildModel()
+{
+    beginResetModel();
+
+    if (m_language.isEmpty()) {
+        GList *sources, *tmp;
+        const gchar *display_name;
+        const gchar *short_name;
+        const gchar *xkb_layout;
+        const gchar *xkb_variant;
+        sources = gnome_xkb_info_get_all_layouts(m_xkbInfo);
+
+        m_layouts.clear();
+        m_layouts.reserve(g_list_length(sources));
+
+        for (tmp = sources; tmp != NULL; tmp = tmp->next) {
+            gnome_xkb_info_get_layout_info(m_xkbInfo, (const gchar *)tmp->data,
+                                           &display_name, &short_name, &xkb_layout, &xkb_variant);
+
+            KeyboardLayoutInfo layout;
+            layout.id = QString::fromUtf8((const gchar *)tmp->data);
+            layout.shortName = QString::fromUtf8(short_name);
+            layout.displayName = QString::fromUtf8(display_name);
+            layout.layoutAndVariant = QString::fromUtf8(g_strconcat(xkb_layout, "+", xkb_variant, NULL));
+
+            if (!layout.shortName.isEmpty()) {
+                qDebug() << "Appending layout with id:" << layout.id;
+                m_layouts.append(layout);
+            }
+
+        }
+        g_list_free(sources);
+        std::sort(m_layouts.begin(), m_layouts.end(), compareLayouts);
+    } else {
+        // TODO
+    }
+
+    endResetModel();
+}
+
+int KeyboardLayoutsModel::rowCount(const QModelIndex &parent) const
+{
+    Q_UNUSED(parent)
+    return m_layouts.count();
+}
+
+QVariant KeyboardLayoutsModel::data(const QModelIndex &index, int role) const
+{
+    const int row = index.row();
+
+    if (row >= m_layouts.count()) {
+        qWarning() << Q_FUNC_INFO << "index out of bounds";
+        return QVariant();
+    }
+
+    KeyboardLayoutInfo layout = m_layouts.at(row);
+
+    switch (role) {
+    case Qt::DisplayRole:
+    case DisplayNameRole:
+        return layout.displayName;
+    case LayoutIdRole:
+        return layout.id;
+    case ShortNameRole:
+        return layout.shortName;
+    case LayoutAndVariantRole:
+        return layout.layoutAndVariant;
+    default: {
+        qWarning() << Q_FUNC_INFO << "unsupported data role";
+        return QVariant();
+    }
+    }
+}
+
+QHash<int, QByteArray> KeyboardLayoutsModel::roleNames() const
+{
+    return m_roleNames;
+}
---
+++ plugins/Wizard/keyboardLayoutsModel.h
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2016 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3, as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranties of
+ * MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef KEYBOARDLAYOUTSMODEL_H
+#define KEYBOARDLAYOUTSMODEL_H
+
+#include <QAbstractListModel>
+
+#define GNOME_DESKTOP_USE_UNSTABLE_API
+#include <libgnome-desktop/gnome-xkb-info.h>
+
+struct KeyboardLayoutInfo;
+
+class KeyboardLayoutsModel: public QAbstractListModel
+{
+    Q_OBJECT
+
+    Q_PROPERTY(QString language READ language WRITE setLanguage NOTIFY languageChanged)
+
+public:
+    explicit KeyboardLayoutsModel(QObject * parent = nullptr);
+    ~KeyboardLayoutsModel();
+
+    enum Roles {
+        LayoutIdRole = Qt::UserRole + 1,
+        DisplayNameRole,
+        ShortNameRole,
+        LayoutAndVariantRole,
+    };
+
+    QString language() const;
+    void setLanguage(const QString &language);
+
+    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+    QHash<int, QByteArray> roleNames() const override;
+
+Q_SIGNALS:
+    void languageChanged(const QString &language);
+
+private Q_SLOTS:
+    void buildModel();
+
+private:
+    QString m_language;
+    QHash<int, QByteArray> m_roleNames;
+    QVector<KeyboardLayoutInfo> m_layouts;
+    GnomeXkbInfo *m_xkbInfo = nullptr;
+};
+
+#endif
