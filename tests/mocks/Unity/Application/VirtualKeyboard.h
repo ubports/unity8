9a78d6d4e4953390dec14f4df5d414e2075d67c7
Daniel d'Andrada <daniel.dandrada@canonical.com>
2016-06-30 10:51:32 -0300

    Make use of MirSurface::inputBounds and drop UbuntuKeyboardInfo hack

--- tests/mocks/Unity/Application/VirtualKeyboard.h
+++ tests/mocks/Unity/Application/VirtualKeyboard.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 Canonical, Ltd.
+ * Copyright (C) 2015,2016 Canonical, Ltd.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -27,6 +27,9 @@ class VirtualKeyboard : public MirSurface
 public:
     explicit VirtualKeyboard();
     ~VirtualKeyboard();
+
+protected:
+    void updateInputBoundsAfterResize() override;
 };
 
 Q_DECLARE_METATYPE(VirtualKeyboard*)
