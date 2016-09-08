4409bac3434619f89c65d4035df4dd509dfb357a
Albert Astals Cid <albert.astals@canonical.com>
2016-09-08 15:52:13 +0200

    Stabilize test

--- tests/plugins/Dash/listviewwithpageheadertest.cpp
+++ tests/plugins/Dash/listviewwithpageheadertest.cpp
@@ -2014,6 +2014,7 @@ private Q_SLOTS:
         QMetaObject::invokeMethod(model, "removeItems", Q_ARG(QVariant, 0), Q_ARG(QVariant, 6));
         QMetaObject::invokeMethod(model, "insertItem", Q_ARG(QVariant, 0), Q_ARG(QVariant, 1000));
         QTRY_COMPARE(lvwph->m_visibleItems.count(), 1);
+        QTRY_COMPARE(lvwph->contentHeight(), 1050.);
         scrollToBottom();
         QMetaObject::invokeMethod(model, "insertItem", Q_ARG(QVariant, 0), Q_ARG(QVariant, 530));
         QTRY_COMPARE(lvwph->m_visibleItems.count(), 2);
