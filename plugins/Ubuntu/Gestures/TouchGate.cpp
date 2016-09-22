c8d52f811b31cc52ab5e7c7cb5e6dd8652130160
Michael Zanetti <michael.zanetti@canonical.com>
2016-09-22 18:18:04 +0200

    add a FIXME on why the assertion is commented out

--- plugins/Ubuntu/Gestures/TouchGate.cpp
+++ plugins/Ubuntu/Gestures/TouchGate.cpp
@@ -61,6 +61,9 @@ void TouchGate::touchEvent(QTouchEvent *event)
         const QTouchEvent::TouchPoint &touchPoint = touchPoints[i];
 
         if (touchPoint.state() == Qt::TouchPointPressed) {
+// FIXME: This assert triggers frequently in make trySomething. We have verified
+// that it's a bug in the mouse to touch conversion of the test environment
+// and not in the actual product. Still, it probably should be cleaned up eventually.
 //            Q_ASSERT(!m_touchInfoMap.contains(touchPoint.id()));
             m_touchInfoMap[touchPoint.id()].ownership = OwnershipRequested;
             m_touchInfoMap[touchPoint.id()].ended = false;
