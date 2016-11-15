2fa3cced9340284de71d0e0ce01aab0dd7194577
Michael Zanetti <michael.zanetti@canonical.com>
2016-11-15 18:14:11 +0100

    catch more exceptions

--- plugins/Unity/Launcher/ualwrapper.cpp
+++ plugins/Unity/Launcher/ualwrapper.cpp
@@ -14,9 +14,14 @@ UalWrapper::UalWrapper(QObject *parent):
 QStringList UalWrapper::installedApps()
 {
     QStringList appIds;
-    for (const std::shared_ptr<Application> &app : Registry::installedApps()) {
-        appIds << QString::fromStdString(app->appId().package) + QStringLiteral("_") + QString::fromStdString(app->appId().appname);
+    try {
+        for (const std::shared_ptr<Application> &app : Registry::installedApps()) {
+            appIds << QString::fromStdString(app->appId().package) + QStringLiteral("_") + QString::fromStdString(app->appId().appname);
+        }
+    } catch(std::runtime_error &e) {
+        qWarning() << "ubuntu-all-launch threw an exception listing apps:" << e.what();
     }
+
     return appIds;
 }
 
@@ -24,24 +29,21 @@ UalWrapper::AppInfo UalWrapper::getApplicationInfo(const QString &appId)
 {
     AppInfo info;
 
-    AppID ualAppId = AppID::find(appId.toStdString());
-    if (ualAppId.empty()) {
-        return info;
-    }
+    try {
+        AppID ualAppId = AppID::find(appId.toStdString());
+        if (ualAppId.empty()) {
+            return info;
+        }
 
-    std::shared_ptr<Application> ualApp;
-    try
-    {
+        std::shared_ptr<Application> ualApp;
         ualApp = Application::create(ualAppId, Registry::getDefault());
-    }
-    catch (std::runtime_error &e)
-    {
-        qWarning() << "Couldn't find application info for" << appId << "-" << e.what();
-        return info;
+
+        info.valid = true;
+        info.name = QString::fromStdString(ualApp->info()->name());
+        info.icon = QString::fromStdString(ualApp->info()->iconPath());
+    } catch(std::runtime_error &e) {
+        qWarning() << "ubuntu-app-launch threw an exception getting app info for appId:" << appId << ":" << e.what();
     }
 
-    info.valid = true;
-    info.name = QString::fromStdString(ualApp->info()->name());
-    info.icon = QString::fromStdString(ualApp->info()->iconPath());
     return info;
 }
