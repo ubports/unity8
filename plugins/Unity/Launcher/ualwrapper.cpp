e842fecd6e83ec97eb02caa8bb997a48a984fcca
Michael Zanetti <michael.zanetti@canonical.com>
2016-11-16 18:15:58 +0100

    fix issues in ualwrapper

--- plugins/Unity/Launcher/ualwrapper.cpp
+++ plugins/Unity/Launcher/ualwrapper.cpp
@@ -16,7 +16,11 @@ QStringList UalWrapper::installedApps()
     QStringList appIds;
     try {
         for (const std::shared_ptr<Application> &app : Registry::installedApps()) {
-            appIds << QString::fromStdString(app->appId().package) + QStringLiteral("_") + QString::fromStdString(app->appId().appname);
+            if (!QString::fromStdString(app->appId().package).isEmpty()) {
+                appIds << QString::fromStdString(app->appId().package) + QStringLiteral("_") + QString::fromStdString(app->appId().appname);
+            } else {
+                appIds << QString::fromStdString(app->appId().appname);
+            }
         }
     } catch(std::runtime_error &e) {
         qWarning() << "ubuntu-all-launch threw an exception listing apps:" << e.what();
@@ -32,15 +36,16 @@ UalWrapper::AppInfo UalWrapper::getApplicationInfo(const QString &appId)
     try {
         AppID ualAppId = AppID::find(appId.toStdString());
         if (ualAppId.empty()) {
+            qWarning() << "Empty ualAppId result for" << appId;
             return info;
         }
 
         std::shared_ptr<Application> ualApp;
         ualApp = Application::create(ualAppId, Registry::getDefault());
 
-        info.valid = true;
         info.name = QString::fromStdString(ualApp->info()->name());
         info.icon = QString::fromStdString(ualApp->info()->iconPath());
+        info.valid = true;
     } catch(std::runtime_error &e) {
         qWarning() << "ubuntu-app-launch threw an exception getting app info for appId:" << appId << ":" << e.what();
     }
