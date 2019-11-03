5b1a0bc77b893f07f00536f21aa7959864b4c356
wdehoog <wdehoog@exalondelft.nl>
2019-11-03 22:30:57 +0100

    more fixes

--- plugins/Lights/Lights.cpp
+++ plugins/Lights/Lights.cpp
@@ -33,7 +33,7 @@ Lights::Lights(QObject* parent)
     lightControl = new QDBusInterface(QStringLiteral("com.ubports.lightcontrol"),
                                      QStringLiteral("/com/ubports/lightcontrol"),
                                      QStringLiteral("com.ubports.lightcontrol"),
-                                     QDBusConnection::systemBus(), this);
+                                     QDBusConnection::(), this);
     turnOff();
 }
 
