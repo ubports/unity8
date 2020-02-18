ubports/unity8

commit b62a87cd6b0825d75af742ac7a457e2d05f8a438

Lock Launcher on screen when no apps are running (#274)
This commit changes the behavior of the auto-hiding Launcher, making it
show whenever there are no apps running and the full-screen indicator
panel is closed.

This fixes what was mostly a design issue, where users could dismiss the
Launcher by tapping on the Background, losing the Launcher entirely.

Additionally, fixes https://github.com/ubports/unity8/issues/133

qml/Shell.qml
@@ -132,10 +132,6 @@ StyledItem {
 
             panel.indicators.hide();
             launcher.hide(launcher.ignoreHideIfMouseOverLauncher);
-        } else if (topLevelSurfaceList.count === 0 && greeter && !greeter.active && !wizard.active) {
-            // Show the Launcher when there are no apps running
-            stage.closeSpread();
-            launcher.show();
         }
 
         // *Always* make sure the greeter knows that the focused app changed
@@ -148,6 +144,15 @@ StyledItem {
     // Note when greeter is waiting on PAM, so that we can disable edges until
     // we know which user data to show and whether the session is locked.
     readonly property bool waitingOnGreeter: greeter && greeter.waiting
+
+    // True when the user is logged in with no apps running
+    readonly property bool atDesktop: topLevelSurfaceList && greeter && topLevelSurfaceList.count === 0 && !greeter.active
+
+    onAtDesktopChanged: {
+        if (atDesktop && stage) {
+            stage.closeSpread();
+        }
+    }
 
     property real edgeSize: units.gu(settings.edgeDragWidth)
 
@@ -396,10 +401,6 @@ StyledItem {
                 if (greeterLoader.toggleDrawerAfterUnlock) {
                     launcher.toggleDrawer(false);
                     greeterLoader.toggleDrawerAfterUnlock = false;
-                }
-                // Show the launcher when there are no running apps
-                else if (topLevelSurfaceList.count < 1) {
-                    launcher.switchToNextState("visible");
                 } else {
                     launcher.hide();
                 }
@@ -516,9 +517,13 @@ StyledItem {
             mode: shell.usageScenario == "desktop" ? "windowed" : "staged"
             minimizedPanelHeight: units.gu(3)
             expandedPanelHeight: units.gu(7)
-            indicatorMenuWidth: parent.width > units.gu(60) ? units.gu(40) : parent.width
-            applicationMenuWidth: parent.width > units.gu(60) ? units.gu(40) : parent.width
+            indicatorMenuWidth: menuWidth
+            applicationMenuWidth: menuWidth
             applicationMenuContentX: launcher.lockedVisible ? launcher.panelWidth : 0
+
+            // Whether the menus should take up the full screen or not
+            readonly property bool partialWidth: parent.width > units.gu(60)
+            readonly property double menuWidth: partialWidth ? units.gu(40) : parent.width
 
             indicators {
                 hides: [launcher]
@@ -573,11 +578,15 @@ StyledItem {
             superPressed: physicalKeysMapper.superPressed
             superTabPressed: physicalKeysMapper.superTabPressed
             panelWidth: units.gu(settings.launcherWidth)
-            lockedVisible: shell.usageScenario == "desktop" && !settings.autohideLauncher && !panel.fullscreenMode
+            lockedVisible: ((shell.usageScenario == "desktop" && !settings.autohideLauncher) || shell.atDesktop) && !collidingWithPanel && !panel.fullscreenMode
             topPanelHeight: panel.panelHeight
             drawerEnabled: !greeter.active
             privateMode: greeter.active
             background: wallpaperResolver.background
+
+            // It can be assumed that the Launcher and Panel would overlap if
+            // the Panel is open and taking up the full width of the shell
+            readonly property bool collidingWithPanel: panel && (!panel.fullyClosed && !panel.partialWidth)
 
             onShowDashHome: showHome()
             onLauncherApplicationSelected: {

tests/qmltests/tst_Shell.qml
@@ -577,7 +577,7 @@ Rectangle {
         function loadShell(formFactor, apps) {
 
             if (apps === undefined) {
-                apps = 0
+                apps = 0;
             }
 
             shellLoader.state = formFactor;
@@ -3238,6 +3238,94 @@ Rectangle {
             mouseClick(menuItem0);
             tryCompare(priv, "currentItem", menuItem0);
             tryCompare(priv.currentItem, "popupVisible", true);
+        }
+
+        function test_launcherLockedWhenNoAppsRunning_data() {
+            return [{
+                tag: "phone",
+                shell: "phone",
+                formFactor: "phone",
+                launcherLocked: false,
+                stateWithOpenIndicators: "",
+                stateWithOpenApps: ""
+            },
+            {
+                tag: "tablet",
+                shell: "tablet",
+                formFactor: "tablet",
+                launcherLocked: false,
+                stateWithOpenIndicators: "visible",
+                stateWithOpenApps: ""
+            },
+            {
+                tag: "desktop without auto-hide",
+                formFactor: "desktop",
+                shell: "desktop",
+                launcherLocked: true,
+                stateWithOpenIndicators: "visible",
+                stateWithOpenApps: "visible"
+            },
+            {
+                tag: "desktop with auto-hide",
+                formFactor: "desktop",
+                shell: "desktop",
+                launcherLocked: false,
+                stateWithOpenIndicators: "visible",
+                stateWithOpenApps: ""
+            },
+            {
+                tag: "desktop with auto-hide at small formFactor",
+                formFactor: "phone",
+                shell: "desktop",
+                launcherLocked: false,
+                stateWithOpenIndicators: "",
+                stateWithOpenApps: ""
+            },
+            {
+                tag: "tablet with auto-hide at small formFactor",
+                formFactor: "phone",
+                shell: "tablet",
+                launcherLocked: false,
+                stateWithOpenIndicators: "",
+                stateWithOpenApps: ""
+            }]
+        }
+
+        /*
+            Ensure that the Launcher will not dismiss while there are no apps
+            running, but gets dismissed when it would be intersected by
+            indicators.
+        */
+        function test_launcherLockedWhenNoAppsRunning(data) {
+            loadShell(data.shell, 0);
+            shellLoader.state = data.formFactor;
+            var launcher = findChild(shell, "launcher");
+            GSettingsController.setAutohideLauncher(!data.launcherLocked);
+            swipeAwayGreeter();
+
+            // Tap somewhere, which would cause the Launcher to hide. There are
+            // no apps running, the Launcher should still be visible.
+            // Wait so all signals get out of the way and the Launcher actually
+            // dismisses in a failure case.
+            tap(shell);
+            wait(100);
+            tryCompare(launcher, "state", "visible");
+            tryCompare(launcher, "x", 0);
+
+            // The Launcher should dismiss at phone size, but not other sizes,
+            // when the indicators open
+            showIndicators();
+            tryCompare(launcher, "state", data.stateWithOpenIndicators);
+            hideIndicators();
+
+            // The Launcher should hide whenever an app starts if auto-hide
+            // is enabled (AKA locked is disabled)
+            addApps(1);
+            tryCompare(launcher, "state", data.stateWithOpenApps);
+
+            // The Launcher should return when all apps are closed
+            killApps();
+            tryCompare(launcher, "state", "visible");
         }
     }
 }

qml/Launcher/Launcher.qml
@@ -108,7 +108,7 @@ FocusScope {
             panel.dismissTimer.stop();
             fadeOutAnimation.stop();
             switchToNextState("visible")
-        } else if (!lockedVisible && state == "visible") {
+        } else if (!lockedVisible && (state == "visible" || state == "drawer")) {
             hide();
         }
     }

qml/Panel/Panel.qml
@@ -33,6 +33,7 @@ import "Indicators"
 Item {
     id: root
     readonly property real panelHeight: panelArea.y + minimizedPanelHeight
+    readonly property bool fullyClosed: indicators.fullyClosed && applicationMenus.fullyClosed
 
     property real minimizedPanelHeight: units.gu(3)
     property real expandedPanelHeight: units.gu(7)

commit 2f80cd2d084ce19cc7d7fa61224b3bb06c1b6c7b

When Launcher is locked, OSK can have full width
b62a87cd caused the Keyboard to appear at partial width when no apps
were running. This was clear when trying to use the Drawer's search or
when in the Wizard.

This commit refactors the Launcher's locking into clearer bindings and
uses those to determine whether the keyboard should have a margin or not

Fixes https://github.com/ubports/ubuntu-touch/issues/1373

qml/Shell.qml
@@ -375,7 +375,7 @@ StyledItem {
         anchors {
             fill: parent
             topMargin: panel.panelHeight
-            leftMargin: launcher.lockedVisible ? launcher.panelWidth : 0
+            leftMargin: (launcher.lockedByUser && launcher.lockAllowed) ? launcher.panelWidth : 0
         }
         z: notifications.useModal || panel.indicators.shown || wizard.active || tutorial.running || launcher.drawerShown ? overlay.z + 1 : overlay.z - 1
     }
@@ -578,7 +578,7 @@ StyledItem {
             superPressed: physicalKeysMapper.superPressed
             superTabPressed: physicalKeysMapper.superTabPressed
             panelWidth: units.gu(settings.launcherWidth)
-            lockedVisible: ((shell.usageScenario == "desktop" && !settings.autohideLauncher) || shell.atDesktop) && !collidingWithPanel && !panel.fullscreenMode
+            lockedVisible: (lockedByUser || shell.atDesktop) && lockAllowed
             topPanelHeight: panel.panelHeight
             drawerEnabled: !greeter.active
             privateMode: greeter.active
@@ -587,6 +587,13 @@ StyledItem {
             // It can be assumed that the Launcher and Panel would overlap if
             // the Panel is open and taking up the full width of the shell
             readonly property bool collidingWithPanel: panel && (!panel.fullyClosed && !panel.partialWidth)
+
+            // The "autohideLauncher" setting is only valid in desktop mode
+            readonly property bool lockedByUser: (shell.usageScenario == "desktop" && !settings.autohideLauncher)
+
+            // The Launcher should absolutely not be locked visible under some
+            // conditions
+            readonly property bool lockAllowed: !collidingWithPanel && !panel.fullscreenMode && !wizard.active
 
             onShowDashHome: showHome()
             onLauncherApplicationSelected: {

tests/qmltests/tst_Shell.qml
@@ -3247,7 +3247,8 @@ Rectangle {
                 formFactor: "phone",
                 launcherLocked: false,
                 stateWithOpenIndicators: "",
-                stateWithOpenApps: ""
+                stateWithOpenApps: "",
+                inputMethodMargin: 0,
             },
             {
                 tag: "tablet",
@@ -3255,7 +3256,8 @@ Rectangle {
                 formFactor: "tablet",
                 launcherLocked: false,
                 stateWithOpenIndicators: "visible",
-                stateWithOpenApps: ""
+                stateWithOpenApps: "",
+                inputMethodMargin: 0,
             },
             {
                 tag: "desktop without auto-hide",
@@ -3263,7 +3265,8 @@ Rectangle {
                 shell: "desktop",
                 launcherLocked: true,
                 stateWithOpenIndicators: "visible",
-                stateWithOpenApps: "visible"
+                stateWithOpenApps: "visible",
+                inputMethodMargin: 64,
             },
             {
                 tag: "desktop with auto-hide",
@@ -3271,7 +3274,8 @@ Rectangle {
                 shell: "desktop",
                 launcherLocked: false,
                 stateWithOpenIndicators: "visible",
-                stateWithOpenApps: ""
+                stateWithOpenApps: "",
+                inputMethodMargin: 0,
             },
             {
                 tag: "desktop with auto-hide at small formFactor",
@@ -3279,7 +3283,8 @@ Rectangle {
                 shell: "desktop",
                 launcherLocked: false,
                 stateWithOpenIndicators: "",
-                stateWithOpenApps: ""
+                stateWithOpenApps: "",
+                inputMethodMargin: 0,
             },
             {
                 tag: "tablet with auto-hide at small formFactor",
@@ -3287,7 +3292,8 @@ Rectangle {
                 shell: "tablet",
                 launcherLocked: false,
                 stateWithOpenIndicators: "",
-                stateWithOpenApps: ""
+                stateWithOpenApps: "",
+                inputMethodMargin: 0,
             }]
         }
 
@@ -3295,11 +3301,17 @@ Rectangle {
             Ensure that the Launcher will not dismiss while there are no apps
             running, but gets dismissed when it would be intersected by
             indicators.
+
+            Additionally, ensure that the keyboard takes up the full width of
+            the Shell when the launcher is locked by Shell functions and has
+            a margin set to avoid the Launcher when the launcher is locked by
+            the user.
         */
         function test_launcherLockedWhenNoAppsRunning(data) {
             loadShell(data.shell, 0);
             shellLoader.state = data.formFactor;
             var launcher = findChild(shell, "launcher");
+            var inputMethod = findChild(shell, "inputMethod");
             GSettingsController.setAutohideLauncher(!data.launcherLocked);
             swipeAwayGreeter();
 
@@ -3326,6 +3338,7 @@ Rectangle {
             // The Launcher should return when all apps are closed
             killApps();
             tryCompare(launcher, "state", "visible");
+            tryCompare(inputMethod.anchors, "leftMargin", data.inputMethodMargin);
         }
     }
 }