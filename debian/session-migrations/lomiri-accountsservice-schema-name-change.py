3fd82c2350b73205557686c1bb55063fce35c147
Ratchanan Srirattanamet <ratchanan@ubports.com>
2021-11-25 22:42:30 +0700

    debian/session-migrations/AS: use .items() to iterate over dict
    Simple iteration will give only keys. One has to use .items() to get
    (key, value) tuples.

--- debian/session-migrations/lomiri-accountsservice-schema-name-change.py
+++ debian/session-migrations/lomiri-accountsservice-schema-name-change.py
@@ -52,7 +52,7 @@ for (src_schema, dst_schema) in AS_SCHEMAS:
     print(f'Migrating {src_schema} to {dst_schema}')
 
     src_props = user_properties_if.GetAll(src_schema)
-    for (key, value) in src_props:
+    for (key, value) in src_props.items():
         user_properties_if.Set(dst_schema, key, value)
 
 marker_path.parent.mkdir(mode=0o755, parents=True, exist_ok=True)
